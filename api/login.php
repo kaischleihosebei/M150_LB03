a4a5fbd87fe18315178e54926261b445a7bed975
kaischleihosebei <49729342+kaischleihosebei@users.noreply.github.com>
2019-04-17 20:09:10 +0200

    Update login.php

--- api/login.php
+++ api/login.php
@@ -1,30 +1,23 @@
 <?php
-// required headers
 header("Access-Control-Allow-Origin: http://localhost/rest-api-authentication-example/");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
-// files needed to connect to database
 include_once 'config/database.php';
 include_once 'objects/user.php';
 
-// get database connection
 $database = new Database();
 $db = $database->getConnection();
 
-// instantiate user object
 $user = new User($db);
 
-// get posted data
 $data = json_decode(file_get_contents("php://input"));
 
-// set product property values
 $user->email = $data->email;
 $email_exists = $user->emailExists();
 
-// generate json web token
 include_once 'config/core.php';
 include_once 'libs/php-jwt-master/src/BeforeValidException.php';
 include_once 'libs/php-jwt-master/src/ExpiredException.php';
@@ -32,7 +25,6 @@ include_once 'libs/php-jwt-master/src/SignatureInvalidException.php';
 include_once 'libs/php-jwt-master/src/JWT.php';
 use \Firebase\JWT\JWT;
 
-// check if email exists and if password is correct
 if($email_exists && password_verify($data->password, $user->password)){
 
     $token = array(
@@ -48,10 +40,8 @@ if($email_exists && password_verify($data->password, $user->password)){
        )
     );
 
-    // set response code
     http_response_code(200);
 
-    // generate jwt
     $jwt = JWT::encode($token, $key);
     echo json_encode(
             array(
@@ -61,14 +51,8 @@ if($email_exists && password_verify($data->password, $user->password)){
         );
 
 }
-
-// login failed
 else{
-
-    // set response code
     http_response_code(401);
-
-    // tell the user login failed
     echo json_encode(array("message" => "Login failed."));
 }
 ?>
