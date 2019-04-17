7dca24f168c4cf3604f1e8536afb843cb679dcf2
kaischleihosebei <49729342+kaischleihosebei@users.noreply.github.com>
2019-04-17 20:14:22 +0200

    Update core.php

--- api/config/core.php
+++ api/config/core.php
@@ -1,11 +1,8 @@
 <?php
-// show error reporting
 error_reporting(E_ALL);
 
-// set your default time-zone
 date_default_timezone_set('Asia/Manila');
 
-// variables used for jwt
 $key = "example_key";
 $iss = "http://example.org";
 $aud = "http://example.com";
